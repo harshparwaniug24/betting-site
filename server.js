d280ce6f5aa3f9e0e5f10b52082fe35be297da20
harsh parwani <harsh.parwani.ug24@nsut.ac.in>
2026-04-22 20:58:40 +0530

    Update server to use environment variable for PORT

--- server.js
+++ server.js
@@ -461,8 +461,7 @@ io.on('connection', socket => {
     });
 });
 
-const PORT = 3000;
-server.listen(PORT, () => {
-    console.log(`\n🎯 Aviator http://localhost:${PORT}\n`);
-    setTimeout(startRound, 2000);
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
 });
